8db988f57dfd3ae2469189bda94461a250c6d0c6
palakbhardwaj684-glitch <palakbhardwaj684@gmail.com>
2026-03-06 22:03:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,13 +126,13 @@ if st.button("Find Careers"):
         
     st.subheader("🤖 AI Career Insight")
     
-   if top_career in career_explanation:
-      st.write(random.choice(career_explanation[top_career]))
+    if top_career in career_explanation:
+        st.write(random.choice(career_explanation[top_career]))
         
    st.subheader("🔮 Recommended Careers")
    st.dataframe(recommendations)
 
-  user_skills = set([s.strip().lower() for s in user_input.split(",")])
+   user_skills = set([s.strip().lower() for s in user_input.split(",")])
 
   for _, row in recommendations.iterrows():
         career = row["Career"]
