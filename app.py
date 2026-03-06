1f1fe01e38fee7c0ed89538d5133791efddb9132
palakbhardwaj684-glitch <palakbhardwaj684@gmail.com>
2026-03-06 22:23:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ data = {
         "HR Manager",
         "Accountant",
         "Digital Marketer"],
-    "Required_Skills": ["Python, Statistics, Machine Learning, Data Visualization",
+    "Required_Skills": [
+        "Python, Statistics, Machine Learning, Data Visualization",
         "HTML, CSS, JavaScript, React",
         "Python, Deep Learning, NLP, TensorFlow",
         "Design Thinking, Figma, UX Research, Creativity",
@@ -24,74 +25,73 @@ data = {
 }
 
 career_roadmap = {
-"Data Scientist": [
-"Learn Python",
-"Learn Statistics",
-"Learn Machine Learning",
-"Practice Data Visualization",
-"Build Data Science Projects"
-],
+    "Data Scientist": [
+       "Learn Python",
+       "Learn Statistics",
+       "Learn Machine Learning",
+       "Practice Data Visualization",
+       "Build Data Science Projects"
+    ],
+    "Web Developer": [
+       "Learn HTML CSS",
+       "Learn JavaScript",
+       "Learn React",
+       "Build Websites",
+       "Deploy Projects"
+    ],
 
-"Web Developer": [
-"Learn HTML CSS",
-"Learn JavaScript",
-"Learn React",
-"Build Websites",
-"Deploy Projects"
-],
+    "AI Engineer": [
+       "Learn Python",
+       "Learn Machine Learning",
+       "Learn Deep Learning",
+       "Work with TensorFlow",
+       "Build AI Projects"
+     ],
 
-"AI Engineer": [
-"Learn Python",
-"Learn Machine Learning",
-"Learn Deep Learning",
-"Work with TensorFlow",
-"Build AI Projects"
-],
+    "Accountant": [
+      "Learn Accounting Basics",
+      "Learn Taxation",
+      "Practice Excel",
+      "Understand Financial Statements",
+      "Use Accounting Software"
+     ],
 
-"Accountant": [
-"Learn Accounting Basics",
-"Learn Taxation",
-"Practice Excel",
-"Understand Financial Statements",
-"Use Accounting Software"
-],
-
-"Digital Marketer": [
-"Learn Social Media Marketing",
-"Learn SEO",
-"Learn Content Marketing",
-"Run Ads Campaign",
-"Analyze Marketing Data"
-]
+    "Digital Marketer": [
+      "Learn Social Media Marketing",
+      "Learn SEO",
+      "Learn Content Marketing",
+      "Run Ads Campaign",
+      "Analyze Marketing Data"
+    ]
 
 }
 
 
 career_explanation = {
-"Data Scientist": [
-"Data Scientists analyze data using machine learning and statistics to find insights.",
-"They work with large datasets and build predictive models for companies."
-],
+    "Data Scientist": [
+     "Data Scientists analyze data using machine learning and statistics to find insights.",
+     "They work with large datasets and build predictive models for companies."
+    ],
 
-"Web Developer": [
-"Web Developers build websites and web applications using programming languages.",
-"They design user interfaces and ensure websites work smoothly."
-],
+    "Web Developer": [
+     "Web Developers build websites and web applications using programming languages.",
+     "They design user interfaces and ensure websites work smoothly."
+    ],
 
-"AI Engineer": [
-"AI Engineers develop artificial intelligence systems using machine learning and deep learning.",
-"They build smart applications like chatbots and recommendation systems."
-],
+    "AI Engineer": [
+     "AI Engineers develop artificial intelligence systems using machine learning and deep learning.",
+     "They build smart applications like chatbots and recommendation systems."
+    ],
 
-"Accountant": [
-"Accountants manage financial records, taxation and company budgets.",
-"They help businesses track income, expenses and financial performance."
-],
+    "Accountant": [
+     "Accountants manage financial records, taxation and company budgets.",
+     "They help businesses track income, expenses and financial performance."
+    ],
 
-"Digital Marketer": [
-"Digital Marketers promote products using online platforms like social media and search engines.",
-"They use marketing strategies to grow business online."
-]
+    "Digital Marketer": [
+     "Digital Marketers promote products using online platforms like social media and search engines.",
+     "They use marketing strategies to grow business online."
+    ]
 }
 df = pd.DataFrame(data)
 
@@ -109,9 +109,7 @@ if st.button("Find Careers"):
     similarity_scores = cosine_similarity(user_vector,skill_matrix).flatten()
 
     df["Match_Score"] = similarity_scores
-
-    df["Match_Score"] = df["Match_Score"] * 100
-    df["Match_Score"] = df["Match_Score"].round(2)
+    df["Match_Score"] = (df["Match_Score"] * 100).round(2)
 
     recommendations = df.sort_values(by="Match_Score", ascending=False).head(3)
     top_career = recommendations.iloc[0]["Career"]
@@ -129,12 +127,12 @@ if st.button("Find Careers"):
     if top_career in career_explanation:
         st.write(random.choice(career_explanation[top_career]))
         
-   st.subheader("🔮 Recommended Careers")
-   st.dataframe(recommendations)
+    st.subheader("🔮 Recommended Careers")
+    st.dataframe(recommendations)
 
-   user_skills = set([s.strip().lower() for s in user_input.split(",")])
+    user_skills = set([s.strip().lower() for s in user_input.split(",")])
 
-  for _, row in recommendations.iterrows():
+    for _, row in recommendations.iterrows():
         career = row["Career"]
         required_skills = set([s.strip().lower() for s in row["Required_Skills"].split(",")])
         missing_skills = required_skills - user_skills
